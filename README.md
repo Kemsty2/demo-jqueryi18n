95ba747afe01ac1a0a3b2fd9595d5808c1d0fa89
kingkemsty <steeve.kemgne@orange.com>
2021-04-25 16:39:39 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Demo Jquery I18n
+
+Une simple démo de la librairie Jquery.I18n. 
+Comment l'utiliser pour internationaliser mon site web.
+
+
+## Authors
+
+- [@kemsty2](https://www.github.com/kemsty2)
+
+  
---
+++ i18n/message.js
@@ -0,0 +1,14 @@
+const messages = {
+    "fr": {
+        "select-text": "Selectionner la langue",
+        "result-text": "Le texte traduit"
+    },
+    "en": {
+        "select-text": "Select the language",
+        "result-text": "The traduct text"
+    },
+    "en": {
+        "select-text": "Sprache auswählen",
+        "result-text": "Der übersetzte Text"
+    }
+}
